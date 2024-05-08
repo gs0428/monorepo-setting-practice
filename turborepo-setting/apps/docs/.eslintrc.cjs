53cac7c4c9fd102c4555e9f6b5b5c941666f6764
gs0428 <lider28@naver.com>
2024-05-09 02:00:06 +0900

    fix: tsconfig.json 파일 경로 찾지 못하는 에러 해결

--- turborepo-setting/apps/docs/.eslintrc.cjs
+++ turborepo-setting/apps/docs/.eslintrc.cjs
@@ -1,4 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["@repo/eslint-config/storybook.js"],
+  parserOptions: {
+    project: "/users/gwangsoo/desktop/monorepo-setting-practice/turborepo-setting/apps/docs/tsconfig.json",
+  },
 };
--- turborepo-setting/packages/ui/.eslintrc.js
+++ turborepo-setting/packages/ui/.eslintrc.js
@@ -1,4 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["@repo/eslint-config/react.js"],
+  parserOptions: {
+    project: "/users/gwangsoo/desktop/monorepo-setting-practice/turborepo-setting/packages/ui/tsconfig.json",
+  },
 };
