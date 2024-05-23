459f14d079bd7ccc7dd642cb59e3de37e23b19ee
gs0428 <lider28@naver.com>
2024-05-23 15:12:37 +0900

    feat: 저장하는 경로에 폴더가 없다면 폴더 생성하는 로직 추가

--- packages/ui/codegen/plop.js
+++ packages/ui/codegen/plop.js
@@ -1,3 +1,7 @@
+import path from "node:path";
+import { cwd } from "node:process";
+import { mkdir } from "node:fs";
+
 export default function (plop) {
   plop.setGenerator("new story", {
     description: "Generate a story about new component ",
@@ -18,6 +22,13 @@ export default function (plop) {
     actions(data) {
       const { isPrimitive } = data;
       const type = isPrimitive ? "primitive" : "themed";
+      const existPath = path.resolve(cwd(), "../..", `apps/docs/stories/${type}`);
+
+      // 경로에 primitive 혹은 themed 폴더가 존재하는지 확인 후
+      // 존재하지 않는다면 폴더 생성
+      if (!existPath) {
+        mkdir(existPath);
+      }
 
       // primitive 혹은 themed일 때 story 저장 경로와 hbs 파일 구분
       const action = {
