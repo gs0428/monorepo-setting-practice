a1f920017ac81388485308ca998d208fa7e7ab0d
gs0428 <lider28@naver.com>
2024-05-23 15:11:45 +0900

    fix: 파일 생성 후 저장되는 경로 수정

--- packages/ui/codegen/plop.js
+++ packages/ui/codegen/plop.js
@@ -3,35 +3,30 @@ export default function (plop) {
     description: "Generate a story about new component ",
     prompts: [
       {
+        // 생성하고자 하는 story의 이름을 입력하는 단계
         type: "input",
         name: "name",
         message: "Enter new component name",
       },
       {
+        // 생성하고자 하는 story가 primitive인지 확인하는 단계
         type: "confirm",
         name: "isPrimitive",
         message: "Is it primitive component?",
       },
     ],
     actions(data) {
-      const actions = [];
+      const { isPrimitive } = data;
+      const type = isPrimitive ? "primitive" : "themed";
 
-      // primitive 컴포넌트이면 primitive hbs 파일로 스토리 생성
-      if (data.isPrimitive) {
-        actions.push({
-          type: "add",
-          path: "../src/{{pascalCase name}}.stories.tsx",
-          templateFile: "templates/story-primitive.hbs",
-        });
-      } else {
-        actions.push({
-          type: "add",
-          path: "../src/{{pascalCase name}}.stories.tsx",
-          templateFile: "templates/story-themed.hbs",
-        });
-      }
+      // primitive 혹은 themed일 때 story 저장 경로와 hbs 파일 구분
+      const action = {
+        type: "add",
+        path: `../../../apps/docs/stories/${type}/{{pascalCase name}}.stories.tsx`,
+        templateFile: `templates/story-${type}.hbs`,
+      };
 
-      return actions;
+      return [action];
     },
   });
 }
