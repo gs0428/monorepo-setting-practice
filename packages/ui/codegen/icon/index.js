b3e58b59be3ec0724d7dd1c2c869db9e51abc8f3
gs0428 <lider28@naver.com>
2024-05-24 16:35:23 +0900

    feat: 덮어쓰는 방식으로 로직 수정

--- packages/ui/codegen/icon/index.js
+++ packages/ui/codegen/icon/index.js
@@ -1,5 +1,4 @@
 import fs from "node:fs/promises";
-import path from "node:path";
 import generateComponent from "./utils/generate-component.js";
 
 async function main() {
@@ -8,17 +7,6 @@ async function main() {
     const result = await fs.readFile("../../../../.icona/icons.json", "utf8");
     // 읽어들인 결과 JSON 형태로 변환
     const svgs = JSON.parse(result);
-
-    // 만들어진 icon 컴포넌트가 쓰여질 디렉토리 경로
-    const icondirPath = path.resolve("../../icon");
-
-    // 존재한다면 한 번 지우기
-    if (await fs.stat(icondirPath).catch(() => false)) {
-      await fs.rm(icondirPath, { recursive: true, force: true });
-    }
-
-    // 해당 경로에 디렉토리 생성
-    await fs.mkdir(icondirPath);
 
     // 모든 svg 파일을 컴포넌트로 생성
     await Promise.all(Object.entries(svgs).map(([name, value]) => generateComponent(name, value.svg)));
