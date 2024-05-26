73c08498bda72fd087290bc6256ab043f6dd7a64
gs0428 <lider28@naver.com>
2024-05-26 15:23:25 +0900

    feat: 아이콘 컴포넌트에 변수 추가하는 로직 구현

--- packages/ui/codegen/icons/index.js
+++ packages/ui/codegen/icons/index.js
@@ -1,24 +1,30 @@
-import fs from "node:fs/promises";
+import fsPromise from "node:fs/promises";
+import fs from "node:fs";
 import path from "node:path";
 import generateComponent from "./utils/generate-component.js";
 
 async function main() {
   try {
     // icons.json 파일 읽기
-    const result = await fs.readFile("../../.icona/icons.json", "utf8");
+    const result = await fsPromise.readFile("../../.icona/icons.json", "utf8");
     // 읽어들인 결과 JSON 형태로 변환
     const svgs = JSON.parse(result);
 
     // 만들어진 icon 컴포넌트가 쓰여질 디렉토리 경로
     const icondirPath = path.resolve("./src/icons");
 
-    // 존재하지 않는다면 디렉토리 생성
-    if (await fs.stat(icondirPath).catch(() => true)) {
-      await fs.mkdir(icondirPath);
+    // 디렉토리가 존재하지 않는다면 생성
+    if (!fs.existsSync(icondirPath)) {
+      await fsPromise.mkdir(icondirPath);
     }
 
     // 모든 svg 파일을 컴포넌트로 생성
     await Promise.all(Object.entries(svgs).map(([name, value]) => generateComponent(name, value.svg)));
+
+    // icon에 대한 컴포넌트까지는 생성 완료.
+    // 현재 방법으로는 size, fill props를 전달하는 방법이 없음
+    // 생성 후 스토리북 생성까지 어떻게 할 것인가?
+    // plop과 hbs 적절히 써보기..
   } catch (err) {
     console.error(err);
   }
--- packages/ui/codegen/icons/utils/generate-component.js
+++ packages/ui/codegen/icons/utils/generate-component.js
@@ -7,14 +7,24 @@ import fs from "node:fs";
  */
 const generateComponent = (name, svg) => {
   const pascalCase = name[0].toUpperCase() + name.split("").splice(1).join("");
+
+  // size와 fill 변수를 이용하기 위해
+  // 고정되어있는 width, height, fill을 변수화 시키는 과정
+  // 단, icons.json에 있는 svg의 크기가 24이고, 색상이 black이라는 가정하에 적용됨
+  const replacedSvg = svg.replace(/"24"/g, "{size}").replace(/fill="black"/g, "fill={fill}");
   fs.writeFile(
     `./src/icons/${name}.tsx`,
-    `function ${pascalCase}() {
-    return (
-    ${svg}
-    );
+    `interface ${pascalCase}Props {
+  size?: number;
+  fill?: string;
 }
-    export default ${pascalCase};
+
+function ${pascalCase}({ size = 24, fill = "black" }: ${pascalCase}Props): React.ReactElement {
+  return (
+  ${replacedSvg});
+}
+
+export default ${pascalCase};
 `,
     "utf-8",
     (err) => {
