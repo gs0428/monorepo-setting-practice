3142eadb526afba2f456d3465e4aeb5a0b312268
gs0428 <lider28@naver.com>
2024-05-31 02:21:20 +0900

    feat: colors 객체 반환하는 함수 구현

---
+++ packages/ui/codegen/colors/utils/generate-color.js
@@ -0,0 +1,24 @@
+/** primary, success, information, ... -> key라고 지칭
+ *  각각의 normal, hover, active, ... -> value라고 지칭
+ *  위 정보들을 포함하도록 객체를 생성하여 반환하는 함수
+ *  ex) primary.normal, success.hover, ...*/
+
+const generateColor = (colors) => {
+  // 반환할 colors를 빈 객체로 생성
+  const colorObj = {};
+
+  // 모든 key에 대해 반복문
+  Object.entries(colors).forEach(([key, value]) => {
+    /** key의 이름으로 빈 객체 생성
+     *  ex) primary = {}*/
+    colorObj[key] = {};
+
+    /** key의 모든 value에 대해 반복문을 돌며 값 넣기
+     *  ex) primary.normal = #FFAE22*/
+    Object.entries(value).forEach(([k, v]) => (colorObj[key][k] = v.value));
+  });
+
+  return colorObj;
+};
+
+export default generateColor;
