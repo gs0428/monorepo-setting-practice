f63e08c21da2e80ab45a15328e4d30d01857e063
gs0428 <lider28@naver.com>
2024-05-31 12:45:57 +0900

    refactor: 변수명 수정

--- packages/ui/codegen/colors/utils/json-to-color-obj.js
+++ packages/ui/codegen/colors/utils/json-to-color-obj.js
@@ -3,19 +3,19 @@
  *  위 정보들을 포함하도록 객체를 생성하여 반환하는 함수
  *  ex) primary.normal, success.hover, ...*/
 
-const jsonToColorObj = (colors) => {
+const jsonToColorObj = (jsonColors) => {
   // 반환할 colors를 빈 객체로 생성
   const colorObj = {};
 
   // 모든 key에 대해 반복문
-  Object.entries(colors).forEach(([key, value]) => {
+  Object.entries(jsonColors).forEach(([key, colors]) => {
     /** key의 이름으로 빈 객체 생성
      *  ex) primary = {}*/
     colorObj[key] = {};
 
-    /** key의 모든 value에 대해 반복문을 돌며 값 넣기
+    /** key의 모든 colors에 대해 반복문을 돌며 값 넣기
      *  ex) primary.normal = #FFAE22*/
-    Object.entries(value).forEach(([k, v]) => (colorObj[key][k] = v.value));
+    Object.entries(colors).forEach(([name, { value }]) => (colorObj[key][name] = value));
   });
 
   return colorObj;
